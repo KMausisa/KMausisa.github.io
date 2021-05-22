36a68b201949e284dbf71d0c4b0e1af7a5614e81
KMausisa <kndrckm18@gmail.com>
2021-05-22 16:53:15 -0700

    js

---
+++ lesson5/js/bom.js
@@ -0,0 +1,17 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const output = document.querySelector(".list");
+
+button.addEventListener("click", () => {
+    let li = document.createElement("li");
+    let deleteButton = document.createElement("button");
+    li.textContent = input.value;
+    button.textContent = "delete";
+    li.append(deleteButton);
+    output.append(li);
+    deleteButton.addEventListener("click", function() {
+        output.removeChild(li);
+        input.textContent = "Add Chapter";
+        input.focus();
+    });
+});
