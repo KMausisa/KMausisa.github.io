369006337f188458e0abbe60bb12b8a2eb26692f
KMausisa <kndrckm18@gmail.com>
2021-05-29 18:33:11 -0700

    moving preston town page folder

---
+++ lesson6/lesson4/js/script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+document.querySelector('#lastUpdate').textContent = `Last Updated: ${document.lastModified}`;
+
+const date = new Date();
+
+const year = date.getFullYear();
+
+document.querySelector('#year').textContent = year;
