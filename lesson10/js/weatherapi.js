57fbd28779d6aa904f6614e05638521f7ccd33bf
KMausisa <kndrckm18@gmail.com>
2021-06-26 19:13:59 -0700

    js

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = 'api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=0d648c9f2d64c1cd646caf6d06eb1492'
+const apiURL = 'api.openweathermap.org/data/2.5/weather?id=5604473&appid=0d648c9f2d64c1cd646caf6d06eb1492&units=imperial';
 
 fetch(apiURL)
   .then((response) => response.json())
