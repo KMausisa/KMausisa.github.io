458242683f9f59bca4ffac40efea1676265e451c
KMausisa <kndrckm18@gmail.com>
2021-06-26 19:18:22 -0700

    fixing url js

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,7 @@
-const apiURL = 'api.openweathermap.org/data/2.5/weather?id=5604473&appid=0d648c9f2d64c1cd646caf6d06eb1492&units=imperial';
+const cityid = '5604473';
+const apiid = '0d648c9f2d64c1cd646caf6d06eb1492';
+
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=0d648c9f2d64c1cd646caf6d06eb1492&units=imperial';
 
 fetch(apiURL)
   .then((response) => response.json())
