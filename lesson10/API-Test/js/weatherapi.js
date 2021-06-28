f75c5d3c193a85851bc2a0c5351c6c40dc6fd5d9
KMausisa <kndrckm18@gmail.com>
2021-06-28 15:19:36 -0700

    js finished

--- lesson10/API-Test/js/weatherapi.js
+++ lesson10/API-Test/js/weatherapi.js
@@ -9,6 +9,7 @@ fetch(apiURL)
     console.log(jsObject);
     document.getElementById('current-temp').textContent = jsObject.main.temp;
     const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    console.log(imagesrc);
     const desc = jsObject.weather[0].description;  // note how we reference the weather array
     document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
     document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
--- lesson10/home page/js/forecast.js
+++ lesson10/home page/js/forecast.js
@@ -6,5 +6,20 @@ const apiURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityid}&ap
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
-      console.log(jsObject);
+    // console.log(jsObject);
+    let day = 0;
+    const dayOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const fiveDayForecast = jsObject.list.filter(forecast => forecast.dt_txt.includes('18:00:00'));
+    console.log(fiveDayForecast);
+
+    // loop through each forecast day
+    fiveDayForecast.forEach(x => {
+        let d = new Date(x.dt_txt);
+        let imagesrc = 'https://openweathermap.org/img/w/' + x.weather[0].icon + '.png';
+        console.log(imagesrc);
+        document.getElementById(`dayOfWeek${day+1}`).textContent = dayOfWeek[d.getDay()];
+        document.getElementById(`forecast${day+1}`).textContent = x.main.temp;
+        document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
+        day++;
+    });
   });
