e04598cff328d1f7c11711f4fa59cfe444141dbb
KMausisa <kndrckm18@gmail.com>
2021-07-01 13:30:31 -0700

    radio js

--- lesson8/gallery/js/radio.js
+++ lesson8/gallery/js/radio.js
@@ -2,10 +2,10 @@ function adjustRating(rating) {
     document.getElementById("ratingvalue").textContent = rating;
 }
 
-function selectResponse() {
-	const s = document.querySelector('#selected')
-	const sel = document.querySelector('#selectbrowser');
-	s.style.display = "block";
-	s.textContent = sel.value;
+// function selectResponse() {
+// 	const s = document.querySelector('#selected')
+// 	const sel = document.querySelector('#selectbrowser');
+// 	s.style.display = "block";
+// 	s.textContent = sel.value;
 	
-}
+// }
