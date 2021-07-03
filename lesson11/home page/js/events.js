ce86cb65d8692988ec0a227efc9287424188471d
KMausisa <kndrckm18@gmail.com>
2021-07-02 21:34:47 -0700

    adding fetch

---
+++ lesson11/home page/js/events.js
@@ -0,0 +1,9 @@
+const cityid = '5604473';
+const apiid = '0d648c9f2d64c1cd646caf6d06eb1492';
+
+const apiURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityid}&appid=${apiid}&units=imperial`;
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+  });
